481aac2fcc58edec8dbdf2102d858622b0d3f723
Aizaz Hassan <aizazhassan1999@gmail.com>
2022-02-25 15:01:01 +0300

    updated get events endpoint

--- routes/events.js
+++ routes/events.js
@@ -9,9 +9,16 @@ const {
 const router = express.Router();
 
 //get all events
-router.get("/", checkAuthenticated, async (_, res) => {
+router.get("/", async (_, res) => {
 	try {
-		const result = await pool.query("SELECT * FROM events");
+		const result = await pool.query(
+			`SELECT e.*, json_agg(u) -> 0 AS user, json_agg(i) AS issues 
+			FROM events AS e 
+			JOIN users AS u ON e.creatorId = u.id 
+			JOIN addressedIssues AS a ON a.eventId = e.id 
+			JOIN issuetypes AS i ON a.issuetypeid = i.id 
+			GROUP BY e.id`,
+		);
 		sendResponse(res, 200, result.rows);
 	} catch (e) {
 		sendError(res, 400, e.message);
