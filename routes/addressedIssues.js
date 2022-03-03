9efd9e05721599705c3690cc3fce6ffae745f3ad
Aizaz Hassan <aizazhassan1999@gmail.com>
2022-03-03 21:07:21 +0300

    added addressedIssues endpoints

--- routes/addressedIssues.js
+++ routes/addressedIssues.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { sendResponse, sendError } = require("../helper");
 const pool = require("../dbPool");
+const { checkAuthenticated, checkIsEventCreator } = require("../middlewares");
 
 const router = express.Router();
 
 //get by eventId
-router.get("/:id", async (req, res) => {
+router.get("/:id", checkAuthenticated, async (req, res) => {
 	const { id } = req.params;
 	try {
 		const results = await pool.query(
@@ -18,8 +19,38 @@ router.get("/:id", async (req, res) => {
 	}
 });
 
-//TODO:
-//add addressed issue to event
+//add new addressed issue to event
+router.post(
+	"/:issueId/event/:eventId",
+	[checkAuthenticated, checkIsEventCreator],
+	async (req, res) => {
+		const { issueId, eventId } = req.params;
+		const query =
+			"INSERT INTO addressedIssues (eventId, issueTypeId) VALUES ($1, $2) RETURNING *";
+		try {
+			const results = await pool.query(query, [eventId, issueId]);
+			sendResponse(res, 200, results.rows[0]);
+		} catch (e) {
+			sendError(res, 400, e.message);
+		}
+	},
+);
+
 //delete addressed issue from event
+router.delete(
+	"/:issueId/event/:eventId",
+	[checkAuthenticated, checkIsEventCreator],
+	async (req, res) => {
+		const { issueId, eventId } = req.params;
+		const query =
+			"DELETE FROM addressedIssues WHERE eventId=$1 AND issueTypeId=$2";
+		try {
+			const results = await pool.query(query, [eventId, issueId]);
+			sendResponse(res, 200);
+		} catch (e) {
+			sendError(res, 400, e.message);
+		}
+	},
+);
 
 module.exports = router;
