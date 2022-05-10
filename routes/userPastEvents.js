30be1d092372d39e7b463e7299e5a910fcff2889
Aizaz Hassan <aizazhassan1999@gmail.com>
2022-05-11 00:24:27 +0300

    updated past events endpoints

--- routes/userPastEvents.js
+++ routes/userPastEvents.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const pool = require("../dbPool");
-const { sendError, sendResponse } = require("../helper");
+const { sendError, sendResponse, getEvents } = require("../helper");
 const { checkAuthenticated } = require("../middlewares");
 
 const router = express.Router();
@@ -8,22 +8,17 @@ const router = express.Router();
 //get user past participated events
 router.get("/participated", checkAuthenticated, async (req, res) => {
 	const userId = req.user.id;
-	const query = `SELECT e.*, json_agg(u) -> 0 AS user, 
-	COALESCE(json_agg(DISTINCT i) FILTER (WHERE i.id IS NOT NULL), '[]') AS issues, 
-	COALESCE(json_agg(DISTINCT jsonb_build_object('id', er.id, 'rule', er.rule)) FILTER (WHERE er.id IS NOT NULL), '[]') AS rules,
-	COALESCE(json_agg(DISTINCT ep) FILTER (WHERE ep.id IS NOT NULL), '[]') AS pictures 
-	FROM events AS e
-	JOIN eventParticipants as p ON e.id=p.eventId AND p.userId=$1 AND e.deletedAt IS NULL
-	JOIN users AS u ON e.creatorId = u.id AND e.deletedAt IS NULL
-	LEFT JOIN eventRules AS er ON er.eventId = e.id
-	LEFT JOIN eventPictures AS ep ON ep.eventId = e.id
-	LEFT JOIN addressedIssues AS a ON a.eventId = e.id
-	JOIN issuetypes AS i ON a.issuetypeid = i.id
-	GROUP BY e.id
-	ORDER BY e.createdAt DESC`;
+	const query = `SELECT e.id 
+	FROM events AS e 
+	JOIN eventParticipants as p 
+	ON e.id=p.eventId AND p.userId=$1
+	WHERE e.deletedAt IS NULL`;
+
 	try {
 		const results = await pool.query(query, [userId]);
-		sendResponse(res, 200, results.rows);
+		const eventIds = results.rows.map((row) => row.id);
+		const eventsResult = await getEvents(eventIds);
+		sendResponse(res, 200, eventsResult.rows);
 	} catch (e) {
 		sendError(res, 400, e.message);
 	}
@@ -32,21 +27,17 @@ router.get("/participated", checkAuthenticated, async (req, res) => {
 //get user past created events
 router.get("/created", checkAuthenticated, async (req, res) => {
 	const userId = req.user.id;
-	const query = `SELECT e.*, json_agg(u) -> 0 AS user, 
-	COALESCE(json_agg(DISTINCT i) FILTER (WHERE i.id IS NOT NULL), '[]') AS issues, 
-	COALESCE(json_agg(DISTINCT jsonb_build_object('id', er.id, 'rule', er.rule)) FILTER (WHERE er.id IS NOT NULL), '[]') AS rules,
-	COALESCE(json_agg(DISTINCT ep) FILTER (WHERE ep.id IS NOT NULL), '[]') AS pictures 
+
+	const updatedQuery = `SELECT e.id
 	FROM events AS e
-	JOIN users AS u ON e.creatorId = u.id AND e.deletedAt IS NULL AND e.creatorId=$1
-	LEFT JOIN eventRules AS er ON er.eventId = e.id
-	LEFT JOIN eventPictures AS ep ON ep.eventId = e.id
-	LEFT JOIN addressedIssues AS a ON a.eventId = e.id
-	JOIN issuetypes AS i ON a.issuetypeid = i.id
-	GROUP BY e.id
-	ORDER BY e.createdAt DESC`;
+	JOIN users AS u ON e.creatorId = u.id AND e.deletedAt IS NULL
+	WHERE e.creatorId=$1 AND e.deletedAt IS NULL`;
+
 	try {
-		const results = await pool.query(query, [userId]);
-		sendResponse(res, 200, results.rows);
+		const results = await pool.query(updatedQuery, [userId]);
+		const eventIds = results.rows.map((row) => row.id);
+		const eventResults = await getEvents(eventIds);
+		sendResponse(res, 200, eventResults.rows);
 	} catch (e) {
 		sendError(res, 400, e.message);
 	}
